dba02fb8ddfa7f88c08cb8c0ca78091c59877fef
unknown <alekssig1@mail.ru>
2022-04-28 23:57:25 +0300

    HTML

--- pythonProjectD5/NewsPortal/NewsPortal/settings.py
+++ pythonProjectD5/NewsPortal/NewsPortal/settings.py
@@ -214,3 +214,41 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'style' : '{',
+    'formatters': {
+        'simple': {
+            'format': '{levelname} {message}'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
